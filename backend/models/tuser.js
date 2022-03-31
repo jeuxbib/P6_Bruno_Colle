45ee7768bb1713438482e96c07a8dae292e773d9
jeuxbib <jeuxbib@gmail.com>
2022-03-31 19:58:17 +0200

    first commit

---
+++ backend/models/tuser.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+// importation du package qui permet de verifier que l'adresse mail de l'utilisateur est unqiue
+const uniqueValidator = require('mongoose-unique-validator');
+
+
+// fonction "schema" de mongoose
+const userSchema = mongoose.Schema({
+    email: { type: String, required: true, unique: true},
+    password: { type: String, required: true }    
+});
+
+userSchema.plugin(uniqueValidator);
+// export du model vers les autres dossiers
+module.exports = mongoose.model('tuser', userSchema);
