560875bf44a1c2e2936ced1500c36cb95343092a
jeuxbib <jeuxbib@gmail.com>
2022-04-15 15:46:05 +0200

    maskdata

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const BDD_NAME = process.env.DB_NAME;
 const CLUSTER = process.env.DB_CLUSTER;
 
 var corsOptions = {
-    origin: "http://127.0.0.1:5500"
+    origin: "http://127.0.0.1:8081"
 };
 
 app.use(cors(corsOptions));
