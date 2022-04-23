7c41590d21d475f500b6210b43ef8288829c9a7a
jeuxbib <jeuxbib@gmail.com>
2022-04-23 12:56:59 +0200

    dependencies

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,17 +1,16 @@
 const express = require('express');
-const router = express.Router();
+const router  = express.Router();
 
 const sauceCtrl = require('../controllers/sauce');
-const auth = require('../middlewares/auth');
-const multer = require('../middlewares/multer-config');
-const validate = require('../middlewares/validate-inputs');
+const auth      = require('../middleware/auth');
+const multer    = require('../middleware/multer-config');
 
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.get('/', auth, sauceCtrl.getAllSauces);
 
-router.get('/', auth, sauceCtrl.getAllSauce);
-router.get('/:id', auth, validate.id, sauceCtrl.getOneSauce);
-router.post('/', auth, multer, validate.sauce, sauceCtrl.createSauce);
-router.put('/:id', auth, multer, validate.id, validate.sauce, sauceCtrl.modifySauce);
-router.delete('/:id', auth, validate.id, sauceCtrl.deleteSauce);
-router.post('/:id/like', auth, validate.id, validate.like, sauceCtrl.likeSauce);
+router.post('/:id/like', auth, sauceCtrl.likeDislike);
 
 module.exports = router;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const userRoutes = require('./routes/user');
 
 const userCtrl = require('../controllers/user');
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+const validate = require('../middlewares/validate-inputs');
+
+router.post('/signup', validate.user, userCtrl.signup);
+router.post('/login', validate.user, userCtrl.login);
 
 module.exports = router;
