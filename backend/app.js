bacccb24b84f3e885d487502aa202f6661b3692a
jeuxbib <jeuxbib@gmail.com>
2022-04-25 15:54:05 +0200

    joi+fs+sanitize

--- backend/app.js
+++ backend/app.js
@@ -18,32 +18,30 @@ app.use(helmet.hidePoweredBy());
 
 const path = require('path');
 
+
 // paths routes
 const saucesRoutes = require("./routes/sauces");
 const usersRoutes = require("./routes/users");
 
-//connexion mongoDB avec mongoose via dotenv pour plus de sécurité
-  mongoose.connect(process.env.DB_CONNECTION,
+mongoose.connect(`mongodb+srv://jeuxbib:jeuxbib@bibcluster.sv8fd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
   { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(()  => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !')); 
-  app.use((req, res, next) => {
+   useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   // Header for same source helmet images
   res.setHeader('Cross-Origin-Resource-Policy', 'same-site');
   next();
-
 });
 
 // base route
 
 app.use("/api/sauces", saucesRoutes);
 app.use("/api/auth", usersRoutes);
-app.use('/images', express.static(path.join(__dirname, 'images'))); 
-
-
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
