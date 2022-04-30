2c4f049e8b1677e36e43fb78b5aedcfe44585e79
jeuxbib <jeuxbib@gmail.com>
2022-04-30 05:01:54 +0200

    mongoose-unique-validator

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,6 @@ mongoose.connect('mongodb+srv://' + process.env.DB_LOGIN + ':' + process.env.DB_
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-<<<<<<< HEAD
 // Connexion a la database de mongoose
 mongoose.connect(`mongodb+srv://jeuxbib:jeuxbib@bibcluster.sv8fd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
   { useNewUrlParser: true,
@@ -40,31 +39,9 @@ app.use((req, res, next) => {
     next();
 });
 
-=======
-const app = express();
-
-app.use(helmet());
-app.use(mongoSanitize({
-    replaceWith: '_'
-}))
-
-// gérer les erreurs CROS, ajout de middleware qui s'appliquera à toute les routes
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); //accéder à notre api depuis n'importe quelle origine '*'
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // ajout des headers mentionnés aux requetes envoyées vers notre api'
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // envoyer les requêtes avec les méthodes get post ...
-    next();
-});
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
-
-//affiche les images sans le path
->>>>>>> 74c627bee75864d75bf52f5b96a9e5f931ea5c1b
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(express.static('images'));
 
-<<<<<<< HEAD
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -74,16 +51,3 @@ app.use('/api/sauces', saucesRoutes)
 
 
 module.exports = app
-=======
-
-app.use('/api/sauces', saucesRoutes)
-app.use('/api/auth', userRoutes);
-
-module.exports = app;
-
-
-
-//motDePassPourLeP6 => userRead
-//mdpPourLeP6 => simon 
-
->>>>>>> 74c627bee75864d75bf52f5b96a9e5f931ea5c1b
